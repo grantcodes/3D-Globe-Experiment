db7ecc334aae0b75d4a0c4ed123fb7d0773d2fb4
Melissa Choy <mel@melissachoy.com>
2021-07-17 22:07:56 +1000

    add stars to background

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
+import { Float32BufferAttribute } from 'three'
 
 const scene = new THREE.Scene()
 
@@ -53,7 +54,26 @@ const group = new THREE.Group()
 group.add(sphere)
 scene.add(group)
 
-camera.position.z = 9 
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff
+})
+
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000
+  const y = (Math.random() - 0.5) * 2000
+  const z = -Math.random() * 2000
+  starVertices.push(x, y, z)
+}
+
+starGeometry.setAttribute('position', 
+  new Float32BufferAttribute(starVertices, 3))
+
+const stars = new THREE.Points(starGeometry, starMaterial)
+scene.add(stars)
+
+camera.position.z = 10 
 
 const mouse = {
   x: undefined,
