2a6b476b6fed1b0386d69441d155b79d8578320a
Melissa Choy <mel@melissachoy.com>
2021-07-17 20:24:07 +1000

    Add Three.js, create scene, sphere & initial shaders for the globe

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 import * as THREE from 'three'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 
 const scene = new THREE.Scene()
-const camera = new THREE.
-      PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
+const camera = new THREE
+      .PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+})
 
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50), 
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader     
+  })
+)
+
+scene.add(sphere)
+
+camera.position.z = 9 
 
 function animate() {
   requestAnimationFrame(animate)
