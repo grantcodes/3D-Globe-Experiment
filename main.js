0aa573590f142239cf1d35137f136aba684f5bae
Melissa Choy <mel@melissachoy.com>
2021-07-17 18:46:13 +1000

    Install three.js package and setup scene

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import './style.css'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene()
+const camera = new THREE.
+      PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+
+animate()
