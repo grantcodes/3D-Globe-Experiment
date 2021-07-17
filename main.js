a29bc3edd7c33e1047138b6cf9fa247ec49187ad
Melissa Choy <mel@melissachoy.com>
2021-07-17 20:55:09 +1000

    Change Globe Texture

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 
 const scene = new THREE.Scene()
+
 const camera = new THREE
       .PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({
-  antialias: true
+  antialias: true,
+  alpha: true 
 })
 
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
+
 document.body.appendChild(renderer.domElement)
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50), 
   new THREE.ShaderMaterial({
     vertexShader,
-    fragmentShader     
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load('./img/globe2.jpg')
+      }
+    }     
   })
 )
 
